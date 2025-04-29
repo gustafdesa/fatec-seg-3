c1d787db5a4c1a2e227bbe2daa7c4856db45f2ca
Gustaf Oliveira <81171856+gustafdesa@users.noreply.github.com>
2025-04-29 11:15:17 +0000

    Initial commit

---
+++ routes/uploadRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const upload = require('../middleware/uploadMiddleware');
+const controller = require('../controllers/uploadController');
+const auth = require('../middleware/authMiddleware');
+
+router.post('/', auth, upload.single('foto'), controller.enviar);
+
+module.exports = router;
